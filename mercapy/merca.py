681219bbc38f6f0b09e7416f9ac66790c70eaabc
Joel Taylor <jtayped@gmail.com>
2024-05-20 21:23:03 +0200

    mercadona object implementation

--- mercapy/merca.py
+++ mercapy/merca.py
@@ -1,97 +1,129 @@
-from .utils.api import query_algolia, fetch_json
-from .constants import *
-from .elements import Product, Season
-
+from typing import Literal
 from urllib.parse import urljoin
 
-
-def search(query: str, lang: str = "es") -> list[Product]:
-    """
-    Queries Mercadona's products using their provider "Algolia".
-
-    Args:
-        query (str): Search query (e.g. Dish Soap).
-        lang (str): Language code. Defaults to spanish. Can be "en" too.
-
-    Rerturns:
-        list[Product]: List of products related to the search.
-    """
-    response = query_algolia(query, lang)
-    hits = response.get("hits", [])
-
-    products = []
-    for h in hits:
-        product = Product(h["id"])
-
-        # The search provider for Mercadona sometimes returns products that don't exists
-        # so before adding them to the list, it must be checked
-        if product.exists():
-            products.append(product)
-
-    return products
+from .constants import WAREHOUSES, MAD1
+from .utils.warehouses import get_warehouse_code
+from .utils.api import *
+from .elements import Product, Season
 
 
-def get_home_recommendations(
-    lang: str = "es", warehouse_postal_code: str = NIJAR_POSTAL_CODE
-) -> dict:
-    """
-    Retrieves product recommendations for the home page grouped by sections.
+class Mercadona:
 
-    Args:
-        lang (str): Language code. Defaults to Spanish. Can be "en" for English.
+    def __init__(
+        self,
+        warehouse: Literal[
+            "mad1",
+            "mad2",
+            "bcn1",
+            "alc1",
+            "vlc1",
+        ] = MAD1,
+        language: Literal["es", "en"] = "es",
+    ) -> None:
+        """
+        Represents a Mercadona warehouse, from where their catalog can browsed.
+        A postcode that isn't a Mercadona warehouse will be treated as a city postcode, and will find the closes warehouse to the population if any.
 
-    Returns:
-        dict: Dictionary where keys are section names (or "layout") and values are lists of recommended products.
-    """
-    url = urljoin(API_URL, f"/api/home/?lang={lang}&wh=4115")
-    response = fetch_json(url)
+        Args:
+            warehouse (str): The target warehouse to retrieve products from. It can also be a city postcode which Mercadona can deliver too. Defaults to "mad1", in Madrid. More warehouses are available in constants.WAREHOUSES.
+            language (str): The language of the information recieved. Defaults to "es": Spanish. Can also be "en": English.
 
-    sections = response.get("sections", [])
+        Raises:
+            ValueError: Raises when a postcode is not in Mercadona's network.
+        """
+        self.language = language
+        self.warehouse = warehouse
 
-    # Dictionary to store products grouped by sections
-    section_products = {}
+        # If the warehouse code is not in the list of warehouses, it means that it
+        # is most probably a postcode.
+        if self.warehouse not in WAREHOUSES:
+            # Get closest warehouse to the postcode
+            self.warehouse = get_warehouse_code(self.warehouse)
 
-    for section in sections:
-        section_name = section.get("layout")
+            # Raise error if the postcode isn't in Mercadona's network
+            if self.warehouse is None:
+                raise ValueError(
+                    "The postcode specified isn't available in Mercadona's network... Try using any of the warehouse codes in constants.WAREHOUSES."
+                )
 
-        items = section.get("content", {}).get("items", [])
+    def search(self, query: str) -> list[Product]:
+        """
+        Queries Mercadona's products using their provider "Algolia".
 
-        for item in items:
-            if item.get("bg_colors", None):
-                parsed_item = Season(item["id"], warehouse_postal_code)
-            else:
-                parsed_item = Product(item, warehouse_postal_code)
+        Args:
+            query (str): Search query (e.g. Dish Soap).
+            lang (str): Language code. Defaults to spanish. Can be "en" too.
 
-            if parsed_item.exists():
-                if section_products.get(section_name, None):
-                    section_products[section_name].append(parsed_item)
+        Rerturns:
+            list[Product]: List of products related to the search.
+        """
+        response = query_algolia(query, self.warehouse, self.language)
+        hits = response.get("hits", [])
+
+        products = []
+        for h in hits:
+            product = Product(
+                h["id"],
+            )
+
+            # The search provider for Mercadona sometimes returns products that don't exists
+            # so before adding them to the list, it must be checked
+            if product.exists():
+                products.append(product)
+
+        return products
+
+    def get_home_recommendations(self) -> dict:
+        """
+        Retrieves product recommendations for the home page grouped by sections.
+
+        Returns:
+            dict: Dictionary where keys are layout names and values are lists of recommended products. The lists of products can also include banners which often are Season objects.
+        """
+        url = urljoin(API_URL, f"/api/home/")
+        response = fetch_json(url, {"lang": self.language, "wh": self.warehouse})
+
+        sections = response.get("sections", [])
+
+        # Dictionary to store products grouped by sections
+        section_products = {}
+
+        for section in sections:
+            section_name = section.get("layout")
+
+            items = section.get("content", {}).get("items", [])
+
+            for item in items:
+                if item.get("bg_colors", None):
+                    parsed_item = Season(item["id"], self.warehouse, self.language)
                 else:
-                    section_products[section_name] = [parsed_item]
+                    parsed_item = Product(item, self.warehouse, self.language)
 
-    return section_products
+                if parsed_item.exists():
+                    if section_products.get(section_name, None):
+                        section_products[section_name].append(parsed_item)
+                    else:
+                        section_products[section_name] = [parsed_item]
 
+        return section_products
 
-def get_new_arrivals(
-    lang: str = "es", warehouse_postal_code: str = NIJAR_POSTAL_CODE
-) -> list[Product]:
-    """
-    New product arrivals at Mercadona
+    def get_new_arrivals(self) -> list[Product]:
+        """
+        New product arrivals at Mercadona
 
-    Args:
-        lang (str): Language code. Defaults to spanish. Can be "en" too.
+        Args:
+            lang (str): Language code. Defaults to spanish. Can be "en" too.
 
-    Returns:
-        list[Product]: List of new product arrivals.
-    """
-    url = urljoin(
-        API_URL, f"/api/home/new-arrivals/?lang={lang}&wh={warehouse_postal_code}"
-    )
-    response = fetch_json(url)
+        Returns:
+            list[Product]: List of new product arrivals.
+        """
+        url = urljoin(API_URL, f"/api/home/new-arrivals/")
+        response = fetch_json(url, {"lang": self.language, "wh": self.warehouse})
 
-    products = []
-    for item in response.get("items", []):
-        product = Product(item)
-        if product.exists():
-            products.append(product)
+        products = []
+        for item in response.get("items", []):
+            product = Product(item)
+            if product.exists():
+                products.append(product)
 
-    return products
+        return products
